43fa3105bc738573f4506dace53099cc2aef15b8
MounirDjiar <mounir@djiar.com>
2021-03-16 15:26:32 +0100

    Add text Bachir

--- Swiftomatic/Swiftomatic/ContentView.swift
+++ Swiftomatic/Swiftomatic/ContentView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-        Text("Mounir")
-            .padding()
-        Text("Djiar ")
-            .padding()
-        
+        VStack {
+            Text("Hello, world!")
+                .padding()
+            Text("Mounir")
+                .padding()
+            Text("Djiar ")
+                .padding()
+            Text("Bachir ")
+                .padding()
+        }
     }
 }
 
