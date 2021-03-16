3fe47b08cc3dee8569a4af68836691f07887b062
bashalir <bachir@sahali.fr>
2021-03-16 15:54:09 +0100

    Add Contributors

---
+++ Swiftomatic/Swiftomatic/Contributors/ContributorsView.swift
@@ -0,0 +1,20 @@
+//
+//  Contributorsiew.swift
+//  Swiftomatic
+//
+//  Created by Bachir SAHALI on 16/03/2021.
+//
+
+import SwiftUI
+
+struct Contributorsiew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Contributorsiew_Previews: PreviewProvider {
+    static var previews: some View {
+        Contributorsiew()
+    }
+}
